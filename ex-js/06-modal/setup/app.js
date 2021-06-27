92ec0003f5ed29e2d5aa3caa158656b5bce19198
ZweiWirlbert <63340848+ZweiWirlbert@users.noreply.github.com>
2021-06-28 04:16:54 +0700

    Complete ex-06 Modal

--- ex-js/06-modal/setup/app.js
+++ ex-js/06-modal/setup/app.js
@@ -2,3 +2,15 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+const modalContent = document.querySelector(".modal-overlay");
+const modalOpen = document.querySelector(".modal-btn");
+const modalClose = document.querySelector(".close-btn");
+
+modalOpen.addEventListener("click", () => {
+  modalContent.classList.add("open-modal");
+});
+
+modalClose.addEventListener("click", () => {
+  modalContent.classList.remove("open-modal");
+});
