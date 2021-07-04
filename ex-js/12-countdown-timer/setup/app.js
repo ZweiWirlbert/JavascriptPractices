3cc33dd911f8b37f13eb0ece0a43ad464ad68ea4
ZweiWirlbert <63340848+ZweiWirlbert@users.noreply.github.com>
2021-07-05 01:09:52 +0700

    add deadline days in ex-12 Countdown

--- ex-js/12-countdown-timer/setup/app.js
+++ ex-js/12-countdown-timer/setup/app.js
@@ -21,3 +21,25 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline-format h4");
+
+// for month, we start with 0 so when we want to add value "October" to month, we use number 9 instead of number 8
+let futureDate = new Date(2021, 9, 24, 17, 30, 0);
+console.log(futureDate);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+
+const date = futureDate.getDate();
+
+const weekday = weekdays[futureDate.getDay()];
+
+month = months[month];
+console.log(month);
+giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`;
